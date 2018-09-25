1cb88b4b22ccd6d4bcb1a6794cd63666d1fa8b67
Caio Ragazzi <ca.ragazzi@gmail.com>
2018-09-25 00:10:24 -0300

    Funcionalidade de envio de email de cobrança.

--- CantinaTioWell/Controllers/AdministrativoController.cs
+++ CantinaTioWell/Controllers/AdministrativoController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -50,10 +51,10 @@ namespace CantinaTioWell.Controllers
             //               select new { NomeCliente = cli.nome, NomeProduto = prod.nome, PrecoProduto = prod.preco };
 
             var clientes = from cli in db.Clientes.ToList()
-                            join comp in db.Compras.ToList() on cli.id equals comp.Cliente.id
-                            join prod in db.Produtoes.ToList() on comp.Produto.id equals prod.id
-                            group prod by new { cli.nome, cli.id } into g
-                            select new { IdCliente = g.Key.id ,NomeCliente = g.Key.nome, Preco = g.Sum(prod => prod.preco) };
+                           join comp in db.Compras.ToList() on cli.id equals comp.Cliente.id
+                           join prod in db.Produtoes.ToList() on comp.Produto.id equals prod.id
+                           group prod by new { cli.nome, cli.id } into g
+                           select new { IdCliente = g.Key.id, NomeCliente = g.Key.nome, Preco = g.Sum(prod => prod.preco) };
 
             List<ClienteCompra> cc = new List<ClienteCompra>();
 
@@ -83,7 +84,7 @@ namespace CantinaTioWell.Controllers
                            join comp in db.Compras.ToList() on cli.id equals comp.Cliente.id
                            join prod in db.Produtoes.ToList() on comp.Produto.id equals prod.id
                            where cli.id == id
-                           select new { NomeCliente = cli.nome, NomeProduto = prod.nome, PrecoProduto = prod.preco };
+                           select new { IdCliente = cli.id ,NomeCliente = cli.nome, NomeProduto = prod.nome, PrecoProduto = prod.preco };
 
             List<ClienteCompra> cc = new List<ClienteCompra>();
 
@@ -91,7 +92,7 @@ namespace CantinaTioWell.Controllers
             {
                 cc.Add(new ClienteCompra
                 {
-                    //IdCliente = item.IdCliente,
+                    IdCliente = item.IdCliente,
                     NomeCliente = item.NomeCliente,
                     NomeProduto = item.NomeProduto,
                     PrecoProduto = item.PrecoProduto
@@ -100,6 +101,38 @@ namespace CantinaTioWell.Controllers
 
 
             return View("ListaClienteEProdutoEspecificoComDivida", cc);
+        }
+
+        public ActionResult EnvioDeEmailDeCobranca(int id)
+        {
+
+            var clientes = from cli in db.Clientes.ToList()
+                           join comp in db.Compras.ToList() on cli.id equals comp.Cliente.id
+                           join prod in db.Produtoes.ToList() on comp.Produto.id equals prod.id
+                           where cli.id == id
+                           select new { IdCliente = cli.id, NomeCliente = cli.nome, NomeProduto = prod.nome, PrecoProduto = prod.preco, EmailCliente = cli.email };
+
+            //Instância classe email
+            MailMessage mail = new MailMessage();
+            mail.To.Add(clientes.First().EmailCliente); 
+            mail.From = new MailAddress("cantinadotiowell@gmail.com");
+            mail.Subject = "Cobrança da Cantina do Tio Well";
+
+            mail.Body = $"Olá {clientes.First().NomeCliente}, esté é um e-mail da Cantina Do Tio Well, informamos que você possui dívidas conosco, favor ajustar.";
+
+            mail.IsBodyHtml = true;
+
+            //Instância smtp do servidor, neste caso o gmail.
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential
+            ("cantinadotiowell@gmail.com", "cantinatiowell");// Login e senha do e-mail.
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+
+            return RedirectToAction("Index");
         }
     }
 }
